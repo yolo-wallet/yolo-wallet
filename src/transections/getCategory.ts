347d242f9df9d8f0a3da08a3fc56b92dd17b6e1a
peacepiece7 <peacepiece7@naver.com>
2023-07-15 20:56:11 +0900

    :recycle: : GROQ query refactoring

--- src/transections/getCategory.ts
+++ src/transections/getCategory.ts
@@ -2,11 +2,19 @@ import client from '@/service/sanity'
 import { YOLO_USER_EXPENSES_DOC_TYPE } from '@/constants/constants'
 import type { Expense } from '@/types/api'
 
+type ExpensesResponse = {
+  item: string
+}
+
 export async function getCategoryByUserIdTransaction(userId: string) {
   try {
-    const userExpenses = await client.fetch(`*[_type == "${YOLO_USER_EXPENSES_DOC_TYPE}" && user._ref == "${userId}"]`)
+    const users: ExpensesResponse[] =
+      await client.fetch(`*[_type == "${YOLO_USER_EXPENSES_DOC_TYPE}" && user._ref == "${userId}"]
+    {
+      "item" : category
+    }`)
     const categories = new Set<string>()
-    userExpenses.forEach((expense: Expense) => categories.add(expense.category))
+    users.forEach((expense) => categories.add(expense.item))
     return [...categories]
   } catch (error) {
     console.error(error)
