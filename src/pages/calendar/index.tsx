279aea4c79ba9a02374b5105cba4ba00598a2d30
beakjiuk <jiuk9987@gmail.com>
2023-07-23 03:04:04 +0900

    캘린더 페이지 캘린더 카테고리 및 금액 창 조절 수정

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,84 +1,103 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 const Alert = dynamic(() => import('antd').then((mod) => mod.Alert), { ssr: false })
 const Calendar = dynamic(() => import('antd').then((mod) => mod.Calendar), { ssr: false })
 import type { Dayjs } from 'dayjs'
 import { useCalendarStore } from '@/store/calenderStore'
 import useUserInfo from '@/hooks/useUserInfo'
 import dynamic from 'next/dynamic'
-import type { CellRenderInfo } from 'rc-picker/lib/interface'
+import dayjs from 'dayjs'
+import { cellRender } from '@/utils/renderCalendarCell'
+import { Card } from 'antd'
 
-const CalendarPage: React.FC = () => {
-  const { daily, categoriesData, getCategories, getExpenses, getCategorieData } = useCalendarStore()
+export default function CalendarPage() {
+  const { monthlyExpenses, fullExpenses, getCalendar, getFullExpenses } = useCalendarStore()
+  const [date, setDate] = useState<string>(dayjs().format('YYYY-MM-DD'))
 
   const [userInfo] = useUserInfo()
   const userId = userInfo.userId
 
-  const [value, setValue] = React.useState<Dayjs | undefined>()
-  const [selectedValue, setSelectedValue] = React.useState<Dayjs | undefined>()
-
-  const onSelect = (newValue: Dayjs) => {
-    setValue(newValue)
-    setSelectedValue(newValue)
-  }
-
-  const onPanelChange = (newValue: Dayjs) => {
-    setValue(newValue)
-  }
-
   useEffect(() => {
     if (!userId) return
-    getCategories(userId)
-    getExpenses('daily', userId)
-    getExpenses('weekly', userId)
-    getExpenses('monthly', userId)
-  }, [userId, getCategories, getExpenses])
+    getFullExpenses(userId)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId])
 
   useEffect(() => {
-    if (categoriesData.length > 0) {
-      getCategorieData(categoriesData[0].categorie, userId)
-      // getCategorieData('undefined', userId)
-    }
-  }, [categoriesData, getCategorieData])
+    getCalendar(dayjs(date).year(), dayjs(date).month() + 1, userId)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId, dayjs(date).month()])
 
-  const dateCellRender = (current: Dayjs) => {
-    const formattedDate = current.format('YYYY-MM-DD')
-    const events = daily.filter((expense) => expense.date === formattedDate)
-    return (
-      <ul className="events">
-        {events.map((expense) => (
-          <li key={expense.id}>
-            {expense.category}: {expense.amount}원
-          </li>
-        ))}
-      </ul>
-    )
+  // * Set the Current Date from Calendar
+  const setDateValue = (selectedDate: Dayjs) => {
+    setDate(selectedDate.format('YYYY-MM-DD'))
   }
 
-  const monthCellRender = (current: Dayjs) => {
-    const formattedMonth = current.format('YYYY-MM')
-    const totalAmount = categoriesData.find((data) => data.categorie === formattedMonth)?.totalAmount
-    return (
-      <div className="notes-month">
-        <section>{totalAmount}</section>
-        <span>Total Amount</span>
-      </div>
-    )
-  }
+  // Get the total amount for the selected date
+  const selectedDateTotalAmount = monthlyExpenses && monthlyExpenses[dayjs(date).date()]
+    ? monthlyExpenses[dayjs(date).date()].reduce((acc, expense) => acc + expense.amount, 0)
+    : 0
 
-  const cellRender = (current: Dayjs, info: CellRenderInfo<Dayjs>) => {
-    if (info.type === 'date') return dateCellRender(current)
-    if (info.type === 'month') return monthCellRender(current)
-    return info.originNode
-  }
 
   return (
     <>
-      {selectedValue && <Alert message={`You selected date: ${selectedValue.format('YYYY-MM-DD')}`} />}
-      <div style={{ maxWidth: 600, margin: '0 auto' }}>
-        <Calendar value={value} cellRender={cellRender} onPanelChange={onPanelChange} onSelect={onSelect} />
+      <div style={{ display: 'flex', maxWidth: 1200, margin: '0 auto' }}>
+        {/* 달력 */}
+        <div style={{ flex: 2 }}>
+          <Calendar
+            value={dayjs(date)}
+            // cellRender를 수정합니다.
+            cellRender={(current, info) => {
+              const selectedDateExpenses = monthlyExpenses && monthlyExpenses[dayjs(current).date()]
+                ? monthlyExpenses[dayjs(current).date()]
+                : []
+
+              const selectedDateTotalAmount = selectedDateExpenses.reduce(
+                (acc, expense) => acc + expense.amount,
+                0
+              )
+
+              // 날짜가 0원이거나 데이터가 없을 경우 null을 반환하여 표시하지 않습니다.
+              if (selectedDateTotalAmount === 0 || selectedDateExpenses.length === 0) {
+                return null
+              }
+
+              return (
+                <div className="ant-fullcalendar-date" key={current.date()}>
+                  <div className="ant-fullcalendar-value" style={{ textAlign: 'right' }}>
+                    {selectedDateTotalAmount.toLocaleString('ko-KR', {
+                      currency: 'KRW',
+                      style: 'currency'
+                    })}
+                  </div>
+                </div>
+              )
+            }}
+            onPanelChange={setDateValue}
+            onSelect={setDateValue}
+          />
+        </div>
+
+        {/* 카드 */}
+        <div style={{ flex: 0.6, marginLeft: '20px' }}>
+          <div style={{ textAlign: 'center', fontSize: '20px', marginBottom: '20px' }}>
+            <Alert message={`You selected date: ${dayjs(date).format('YYYY-MM-DD')}`} />
+          </div>
+
+          {monthlyExpenses && monthlyExpenses[dayjs(date).date()] && (
+            <div style={{ fontSize: '16px', paddingLeft: '20px' }}>
+              <Card style={{ width: '100%' }}>
+                {monthlyExpenses[dayjs(date).date()].map((expense) => (
+                  <div key={expense.id}>
+                    <p>
+                      <strong>{expense.category}</strong>: {expense.amount.toLocaleString('ko-KR', { currency: 'KRW', style: 'currency' })}
+                    </p>
+                  </div>
+                ))}
+              </Card>
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
 }
-
-export default CalendarPage
--- src/store/calenderStore.ts
+++ src/store/calenderStore.ts
@@ -1,70 +1,33 @@
 import { create } from 'zustand'
 import api from '@/clientAPI'
-import dayjs from 'dayjs'
-import { Expense, ExpensePeriod, ExpenseSummary } from '@/types/api'
+import { Expense, ExpenseCalendar } from '@/types/api'
 
 interface CalendarStore {
-  categoriesData: { categorie: string; totalAmount: number }[]
-  daily: Expense[]
-  weekly: Expense[]
-  monthly: Expense[]
-  getCategories: (userId: string) => void
-  getExpenses: (period: ExpensePeriod, userId: string) => void
+  fullExpenses: Expense[]
+  monthlyExpenses: ExpenseCalendar
   getCalendar: (year: number, month: number, userId: string) => void
-  getCategorieData: (categorie: string, userId: string) => void
+  getFullExpenses: (userId: string) => void
 }
 
 export const useCalendarStore = create<CalendarStore>((set) => ({
-  categoriesData: [],
-  daily: [],
-  weekly: [],
-  monthly: [],
-  getCategories: async (userId: string) => {
-    const { data } = await api.get<string[]>(`/api/categories?userId=${userId}`)
-    set({ categoriesData: data.map((categorie) => ({ categorie, totalAmount: 0 })) })
-  },
+  fullExpenses: [] as Expense[],
+  monthlyExpenses: {} as ExpenseCalendar,
 
-  getExpenses: async (period: ExpensePeriod, userId: string) => {
-    const { data } = await api.get<ExpenseSummary>(`/api/expenses/summary?period=${period}&userId=${userId}`)
-    set({ [period]: data })
-  },
+  // * 월별 소비 목록을 가져온다.
   getCalendar: async (year: number, month: number, userId: string) => {
-    const { data } = await api.get<Expense[]>(`api/expenses/calendar?year=${year}&month=${month}&userId=${userId}`)
-
-    let oneMonthCalender: Expense[] = []
-    for (let key in data) {
-      oneMonthCalender = oneMonthCalender.concat(data[key])
-    }
-
-    let categories: string[] = oneMonthCalender.map((data) => data.category)
-
-    let categoriesData: { categorie: string; totalAmount: number }[] = []
-
-    for (let i = 0; i < categories.length; i++) {
-      categoriesData[i] = { categorie: categories[i], totalAmount: 0 }
-      oneMonthCalender.forEach((data: Expense) => {
-        if (data.category === categories[i]) {
-          categoriesData[i].totalAmount += data.amount
-        }
-      })
-    }
-
-    categoriesData.sort((a, b) => b.totalAmount - a.totalAmount)
-
+    const { data } = await api.get<ExpenseCalendar>(
+      `api/expenses/calendar?year=${year}&month=${month}&userId=${userId}`
+    )
     set({
-      daily: oneMonthCalender,
-      categoriesData: categoriesData
+      monthlyExpenses: data
     })
   },
-  getCategorieData: async (categorie: string, userId: string) => {
-    const res = await api.get<Expense[]>(`/api/expenses/search?q=${categorie}&userId=${userId}`)
 
-    res.data.sort((a, b) => dayjs(a.date).valueOf() - dayjs(b.date).valueOf())
-
-    if (categorie === 'undefined') {
-      set({ daily: res.data })
-    } else {
-      set({ daily: res.data })
-    }
+  // * 전체 소비 목록 가져을 가져온다.
+  getFullExpenses: async (userId: string) => {
+    const { data } = await api.get<Expense[]>(`api/expenses/search?userId=${userId}`)
+    set({
+      fullExpenses: data
+    })
   }
 }))
